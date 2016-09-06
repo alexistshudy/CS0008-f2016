aa85a36e729041476272e2744dc65eebb551c5a9
alexistshudy <abtshudy@othot.com>
2016-09-06 14:50:01 -0400

    Executed Exercise 3 from Chapter 1

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,15 @@
+#
+# Template for code submission
+# name: Alexis Tshudy
+# email: abt21@pitt.edu
+# date: 03/09/2016
+# class: CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+
+# Description:
+# description of this file goes here
+# Example: Starting with Python, Chapter 1, Exercise 3
+#
+# Notes:
+# any notes to the instructor and/or TA goes here
+# ...and now let's program with Python
