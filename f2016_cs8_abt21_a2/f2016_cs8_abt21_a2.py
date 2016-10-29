f11ddb227d47ae129eb92e9233de8e3f5bd55fbb
alexistshudy <abtshudy@othot.com>
2016-10-29 18:05:15 -0400

    Assignment 2 - Alexis Tshudy

--- f2016_cs8_abt21_a2/f2016_cs8_abt21_a2.py
+++ f2016_cs8_abt21_a2/f2016_cs8_abt21_a2.py
@@ -5,25 +5,18 @@
 # instructor : Max Novelli (man8@pitt.edu)
 # description: Assignment 2 - Python Script
 
-# Ask user for file name; indicate instructions for closing the program.
-
 #define function: printKV(key,value,klen=0)
 def printKV(key,value,klen=0):
     #define the "width" as the maximum of the key itself or the specified key
     width = max(len(key),klen)
     widthvalue = str(width)
-    #define value formatting depending on variable type of value
+    #define value formatting depending on variable type of value using isinstance
     if isinstance(value, str):
         print(format(key,widthvalue),":",format(value,'20'))
     elif isinstance(value, int):
         print(format(key,widthvalue), ":", format(value, '10,d'))
     elif isinstance(value, float):
         print(format(key,widthvalue), ":", format(value, '10,.3f'))
-
-
-userinput = str(input("Please enter the name of the file you would like to use. Or to quit, please enter 'quit' or 'q'"))
-overall_distance = 0
-overall_linecount = 0
 
 #define function: processFile(fh)
 def processFile(fh):
@@ -33,34 +26,47 @@ def processFile(fh):
     # initialize counter and agregator variables at 0
     line_count = 0
     total_distance = 0
-    #need to fix...
+    #For loop, runs through each line, adds one to counter and adds distance column to total_distance
     for line in open_file:
-        #use rstrip to remove new line
+        #use .rstrip to remove new line created at the end
         line = line.rstrip("\n")
-        #split string using .split
+        #split string using .split; split on comma deliminator
         split_line = line.split(",")
         #assign second column to distance variable and convert to float
         distance = float(split_line[1])
-        #add one to counter and at distance to total distance
+        #add one to counter and add distance to total distance
         line_count += 1
         total_distance += distance
-    printKV("File Name",filename,20)
-    printKV("Partial Total # of Lines",line_count,20)
-    printKV("Partial distance run",total_distance,20)
 
+    #print file-specific totals using printKV formatting
+    printKV("File Name",filename,25)
+    printKV("Partial Total # of Lines",line_count,25)
+    printKV("Partial distance run",total_distance,25)
+
+    #add file-level totals to overall distance and linecounts, overall variables are global vars
     global overall_distance
     global overall_linecount
     overall_distance += total_distance
     overall_linecount += line_count
-    print("Totals")
-    printKV("Total Number of Lines",overall_linecount,20)
-    printKV("Total Distance Run", overall_distance,20)
 
+    #print using printKV format for Totals
+    print("Totals")
+    printKV("Total Number of Lines",overall_linecount,25)
+    printKV("Total Distance Run", overall_distance,25)
+
+    #close the file
+    open_file.close()
+
+# Start Main script
+#ask for user input, includes instructions for stopping/quiting the program
+userinput = str(input("Please enter the name of the file you would like to use. Or to quit, please enter 'quit' or 'q'"))
+overall_distance = 0
+overall_linecount = 0
+
+#while loop tests user input & calls processFile function
 while userinput != "quit" and userinput != "q" and userinput != "":
     filename = userinput
     processFile(filename)
-    userinput = str(input("Please enter the name of the file you would like to use. Or to quit, please enter 'quit' or 'q'"))
-
-
+    userinput = str(input("Would you like to process another file? Please enter the name of the file you would like to use. Or to quit, please enter 'quit' or 'q'"))
 
 
