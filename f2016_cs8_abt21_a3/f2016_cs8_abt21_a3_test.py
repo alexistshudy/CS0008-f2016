08fab1133f477845b20ff55b7a8f8fe2e383e4ac
alexistshudy <abtshudy@othot.com>
2016-11-16 11:52:28 -0500

    Assignment3 (asof11/16) updated to include accumulator variables & output
    Still missing max & min

--- f2016_cs8_abt21_a3/f2016_cs8_abt21_a3_test.py
+++ f2016_cs8_abt21_a3/f2016_cs8_abt21_a3_test.py
@@ -1,11 +1,38 @@
-import pprint
-pp = pprint.PrettyPrinter()
-
 from collections import defaultdict
-masterfile = 'f2016_cs8_a3.data.txt'
 
+# length of the key
+FORMAT_KEY_LENGTH = 30
+
+#define function: printKV(key,value,klen=0)
+def printKV(key,value,klen=0):
+    #define the "width" as the maximum of the key itself or the specified key
+    width = max(len(key),klen)
+    widthvalue = str(width)
+    #define value formatting depending on variable type of value using isinstance
+    if isinstance(value, str):
+        print(format(key,widthvalue),":",format(value,'20'))
+    elif isinstance(value, int):
+        print(format(key,widthvalue), ":", format(value, '12,d'))
+    elif isinstance(value, float):
+        print(format(key,widthvalue), ":", format(value, '12,.5f'))
+
+#initialize overall accumulator variables, to be updated with each new file input
+overall_filecount = 0
+overall_distance = 0
+overall_line_count = 0
+
+#initialize participant count accumulator variables
+repeat_count = 0
+participant_count = 0
+
+
+masterfile = 'f2016_cs8_a3.data.txt'
+#create a list of file names from masterfile
+#initialize empty list
 filenames = []
+#open masterfile
 open_masterinput = open(masterfile)
+#each line in the masterfile is a file name, append each filename to the empty list, filenames
 for line in open_masterinput:
     filename = line.strip()
     filenames.append((filename))
@@ -17,6 +44,9 @@ for line in open_masterinput:
 records = []
 #read each filename from the master input and open the file
 for filename in filenames:
+    #initialize partial accumulator variables
+    line_count = 0
+    total_distance = 0
     open_file = open(filename)
     #skip the header row
     next(open_file)
@@ -25,14 +55,22 @@ for filename in filenames:
         #assign first column to name and second column to dist, use strip and split to create two distinct columns from the csv format
         name, dist = line.strip().split(',')
         dist = float(dist)
+        ##add one to counter and add dist to total distance
+        line_count += 1
+        total_distance += dist
         #append name & distance to the records list
         records.append((name,dist))
+    #add to overall accumulator variables
+    overall_filecount += 1
+    overall_line_count += line_count
+    overall_distance += total_distance
     #close the file
     open_file.close()
 
-#print records as a test
-#print('Printing list "records":')
-#pp.pprint(records)
+#print processing summary (# of input files read, number of lines read, total distance run)
+printKV('Number of input files read',overall_filecount,FORMAT_KEY_LENGTH)
+printKV('Total number of lines read',overall_line_count,FORMAT_KEY_LENGTH)
+printKV('Total distance run',overall_distance,FORMAT_KEY_LENGTH)
 
 #Casts d as a dictionary using the defaultdict function from collections
 distance_dict= defaultdict(list)
@@ -41,19 +79,22 @@ for name, dist in records:
     #uses collections module to append new names as new keys & for repeat names append additional dist values to the list for the same key
     distance_dict[name].append(dist)
 
-#print the dictionary as a test
-#print('Printing dictionary "distance_dict":')
-#pp.pprint(distance_dict)
-
 output_dict = defaultdict(dict)
+
 for k,v in distance_dict.items():
+    participant_count += 1
     # count number of values per key (# of times each name appears in master list)
     count = (len(v))
+    if count >1:
+        repeat_count += 1
+    # sum distances
     ind_total_dist = sum(v)
+    #create output as 'k' :
     output_dict[k] = [count,ind_total_dist]
 
-#print('Printing dictionary "d":')
-#pp.pprint(d)
+#print participant counts & repeat counts
+print(participant_count)
+print(repeat_count)
 
 #output data file with name, count, and total distance for each participant
 import csv
@@ -62,4 +103,3 @@ with open('outputfile.csv', 'w') as f:
     c.writerow(['name','count','total distance'])
     for key, value in output_dict.items():
         c.writerow([key] + value)
-
